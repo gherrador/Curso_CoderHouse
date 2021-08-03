64c68636ed9724a5b4db140df133bddaef94122d
gherrador <82460834+gherrador@users.noreply.github.com>
2021-08-02 21:42:51 -0300

    Add files via upload

---
+++ 13. Websocket - Parte 2/public/chat.js
@@ -0,0 +1,40 @@
+const { promises: { readFile, writeFile, unlink } } = require('fs');
+
+class chat {
+    constructor() {}
+
+    leer = async() => {
+        try {
+            const contenido = await readFile('./mensajes/chats.txt', 'utf-8')
+            return (JSON.parse(contenido))
+        } catch (err) {
+            return [];
+        }
+    }
+    borrar = async() => {
+        try {
+            await unlink('./mensajes/chats.txt')
+        } catch (err) {
+            console.log('imposible borrar')
+        }
+    }
+
+    guardar = async(autor, texto) => {
+        try {
+            let mensaje = await this.leer()
+            const nuevoMensaje = {
+                autor,
+                texto,
+                id: mensaje.length + 1
+            };
+            nuevoMensaje.date = new Date().toLocaleString();
+            mensaje.push(nuevoMensaje);
+            await writeFile('./mensajes/chats.txt', JSON.stringify(mensaje, null, '\t'));
+            return `Se ha agregado el autor ${autor}`;
+        } catch (err) {
+            console.log('Algo salio mal... fue imposible guardar el mensaje');
+        }
+    }
+}
+
+module.exports = chat;

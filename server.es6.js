7dfea05f6a3dc9b21a6e4a5f2b9a3961b81776ec
gherrador <82460834+gherrador@users.noreply.github.com>
2021-08-18 17:13:29 -0300

    Update server.es6.js

--- server.es6.js
+++ server.es6.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path')
 const handlebars = require('express-handlebars');
 const productoRoutes = require('./routes/productos');
+require("babel-core/register");
+require("babel-polyfill");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));

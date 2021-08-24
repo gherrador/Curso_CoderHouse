eade29a580533696053377da91a1c710e1a222f2
gherrador <82460834+gherrador@users.noreply.github.com>
2021-08-24 16:22:48 -0300

    proyecto final - primera entrega

---
+++ api/producto.js
@@ -0,0 +1,68 @@
+class Producto {
+    constructor() {
+        this.productos = [];
+    }
+
+    guardar(producto) {
+        producto.id = this.productos.length + 1
+        this.productos.push({
+            id: producto.id,
+            timestamp: Date.now(),
+            nombre: producto.nombre,
+            descripcion: producto.descripcion,
+            codigo: producto.codigo,
+            foto: producto.foto,
+            precio: producto.precio,
+            stock: producto.stock
+        });
+        return producto
+    }
+
+    listar = async() => {
+        try {
+            if (this.productos.length < 1) return false
+            return this.productos;
+        } catch (err) {
+            console.log('Sucedio un error inesperado', err);
+        }
+    }
+
+    listarPorId(id) {
+        const producto = this.productos.filter((producto) => producto.id == id)[0]
+        if (!producto) {
+            throw new Error('producto no encontrado');
+        }
+        return producto
+    }
+
+    actualizarPorId(id, productonuevo) {
+        try {
+            this.productos = this.productos.map((producto) => {
+                if (producto.id === parseInt(id)) {
+                    producto.nombre = productonuevo.nombre
+                    producto.precio = parseInt(productonuevo.precio)
+                    producto.descripcion = productonuevo.descripcion
+                    producto.codigo = productonuevo.codigo
+                    producto.foto = productonuevo.foto
+                    producto.stock = productonuevo.stock
+                }
+                return producto;
+            });
+            return true;
+        } catch (err) {
+            console.log('Ups. algo paso'.err);
+        }
+    }
+
+    borrarPorId(id) {
+        let producto = this.productos.filter((producto) => producto.id == id)
+        let productoABorrar = this.productos.filter((producto) => producto.id != id)
+        if (!producto) {
+            throw new Error('producto no encontrado');
+        }
+        this.productos = productoABorrar
+        return producto
+    }
+}
+
+module.exports = Producto

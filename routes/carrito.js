4df9101c9fab90fc10551bb5c2115206b4a1c575
gherrador <82460834+gherrador@users.noreply.github.com>
2021-08-28 16:21:19 -0300

    Add files via upload

---
+++ routes/carrito.js
@@ -0,0 +1,48 @@
+const express = require('express');
+// Para Carrito en  momemoria usar:
+//const Carrito = require('../api/carritomemoria')
+//y quitar el "New CarritoFS"
+
+//---------------------------------------------------
+
+// Para carrito en persistencia Filesystem usar:
+const CarritoFS = require('../api/carritofs')
+let Carrito = new CarritoFS
+const app = express();
+const routerCarrito = express.Router()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+routerCarrito.post('/agregar/:id', async(req, res) => {
+    let producto_nuevo = await Carrito.guardar(req.params.id); {
+        res.status(201).json(producto_nuevo)
+    }
+});
+
+routerCarrito.get("/listar", async(req, res) => {
+    const products = await Carrito.listar()
+    if (!products) {
+        return res.status(404).json({
+            error: "no hay productos cargados",
+        });
+    }
+    res.json(products)
+});
+
+
+routerCarrito.get('/listar/:id', async(req, res) => {
+    const products = await Carrito.listarPorId(req.params.id)
+    if (!products) {
+        return res.status(404).json({
+            error: "No se encontro el producto solicitado",
+        });
+    }
+    res.json(products);
+});
+
+routerCarrito.delete('/borrar/:id', (req, res) => {
+    const borrar = Carrito.borrarPorId(req.params.id)
+    res.status(200).send(borrar);
+});
+
+module.exports = routerCarrito;

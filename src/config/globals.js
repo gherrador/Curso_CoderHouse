b87c6ff8f9707d87de3491abcd8ae7564411eee9
gherrador <82460834+gherrador@users.noreply.github.com>
2021-09-01 19:31:33 -0300

    Delete src directory

--- src/config/globals.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config()
-
-module.exports = {
-    PORT: process.env.PORT || 9000,
-    NODE_ENV: process.env.NODE_ENV,
-}
--- src/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const routes = require('./routes/routes')
-const router = express.Router()
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(routes(router))
-module.exports = app;

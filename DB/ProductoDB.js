cb4e9b2f727861f01ec96a53ded2fa323da13b00
gherrador <82460834+gherrador@users.noreply.github.com>
2021-09-01 22:33:37 -0300

    entrega 20

---
+++ DB/ProductoDB.js
@@ -0,0 +1,100 @@
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017/ecommerce';
+
+const esquemaProducto = new mongoose.Schema({
+    id: { type: Number, require: true },
+    title: { type: String, require: true, max: 100 },
+    price: { type: String, require: true, max: 100 },
+    thumbnail: { type: String, require: true, max: 100 }
+})
+
+const daoProductos = mongoose.model('productos', esquemaProducto);
+
+class ProductoDB {
+    constructor() {
+        mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        }, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Conectado a la base en constructor de productoDb');
+            }
+        })
+    }
+
+    crearTabla() {
+        return daoProductos.create({}, (err, res) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(res);
+            }
+        });
+    }
+
+    guardar(productos) {
+        return daoProductos.create(productos, (err, res) => {
+            if (err) {
+                console.log(err);
+            } else {
+                return true;
+            }
+        });
+    }
+
+    listar() {
+        return daoProductos.find({}, (err, res) => {
+            if (err) {
+                console.log(err)
+            } else {
+                return false
+            }
+        })
+    }
+
+    listarPorId(id) {
+        return daoProductos.find({ id: id }, (err, res) => {
+            if (err) {
+                console.log(err)
+            } else {
+                return true
+            }
+        });
+    }
+
+    borrarPorId(id) {
+        return daoProductos.deleteOne({ id: id }, (err, res) => {
+            if (err) {
+                console.log('Imposible borrar producto')
+            } else {
+                return true
+            }
+        });
+    }
+    actualizarPorId(id, data) {
+
+        const nuevoTitle = data.title;
+        const nuevoPrice = data.price;
+        const nuevothumbnail = data.thumbnail;
+
+        return daoProductos.updateOne({ id: id }, {
+            $set: {
+                title: nuevoTitle,
+                price: nuevoPrice,
+                thumbnail: nuevothumbnail
+            }
+        }, (err, res) => {
+            if (err) {
+                console.log(err)
+            } else {
+                return true;
+            }
+        });
+    }
+}
+
+module.exports = ProductoDB;
---
+++ src/controllers/producto.js
@@ -0,0 +1,50 @@
+class Producto {
+    constructor() {
+        this.productos = [];
+    }
+
+    guardar(producto) {
+        producto.id = this.productos.length + 1
+        this.productos.push(producto)
+        return producto
+    }
+
+
+    listar() {
+        if (this.productos.length == 0) {
+            return false;
+        }
+        return this.productos
+    }
+
+    obtenerPorId(id) {
+        const producto = this.productos.filter((producto) => producto.id == id)[0]
+        if (!producto) {
+            throw new Error('producto no encontrado');
+        }
+        return producto
+    }
+
+    actualizarPorId(id, productonuevo) {
+        for (let i = 0; i < this.productos.length; i++) {
+            if (this.productos[i].id == id) {
+                this.productos[i] = productonuevo
+                productonuevo.id = JSON.parse(id)
+                return productonuevo
+            }
+        }
+        throw new Error('No existe el producto a actualizar')
+    }
+
+    borrarPorId(id) {
+        let producto = this.productos.filter((producto) => producto.id == id)
+        let productoABorrar = this.productos.filter((producto) => producto.id != id)
+        if (!producto) {
+            throw new Error('producto no encontrado');
+        }
+        this.productos = productoABorrar
+        return producto
+    }
+}
+
+module.exports = Producto
